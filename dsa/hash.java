7b4b20909aea3f1859b5811266291d2b14671640
Praneeth <praneeth.nkl@gmail.com>
2024-08-05 16:33:35 +0530

    Add Hashtable

---
+++ dsa/hash.java
@@ -0,0 +1,19 @@
+package dsa;
+
+import java.util.*;  
+public class hash
+{  
+    /* Driver Code */  
+    public static void main(String args[])  
+    {  
+       /* Create a HashTable to store String values corresponding to integer keys */  
+        Hashtable<Integer, String> hm = new Hashtable<Integer, String>();  
+        /* Input the values */  
+        hm.put(3, "You are visiting");  
+        hm.put(5, "Hello");  
+        hm.put(9, "website");  
+        hm.put(2, "Javatpoint");  
+        /* Display the Hashtable */  
+        System.out.println(hm);  
+    }  
+}  
